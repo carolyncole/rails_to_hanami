9a0e706a29a7f3795dbd021aedc69d1158bdb49e
Carolyn Cole <cac9@princeton.edu>
2026-05-19 15:50:19 -0400

    working code for editing a book

--- bookshelf/app/views/books/edit.rb
+++ bookshelf/app/views/books/edit.rb
@@ -4,6 +4,17 @@ module Bookshelf
   module Views
     module Books
       class Edit < Bookshelf::View
+        include Deps["repos.book_repo"]
+
+        expose :book do |context:, id:|
+          book_repo.get(id)
+        end
+        expose :form_submit, default: "Update Book"
+        expose :form_method, default: "PATCH"
+
+        expose :form_path do |context:, id:|
+          context.routes.path(:book, id: id)
+        end
       end
     end
   end
