8ef1ddc08b07f273525823a5085aff902dc3eebe
Carolyn Cole <cac9@princeton.edu>
2026-05-19 10:21:24 -0400

    Handeling errors in hanami create and edit forms

--- bookshelf/app/views/books/new.rb
+++ bookshelf/app/views/books/new.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 module Bookshelf
   module Views
     module Books
@@ -15,6 +14,9 @@ module Bookshelf
         expose :form_method, default: "POST"
         expose :form_path do |context:| 
           context.routes.path(:books)
+        end
+        expose :errors do |context:|
+          context.request.params.errors[:book] || {}
         end
       end
     end
