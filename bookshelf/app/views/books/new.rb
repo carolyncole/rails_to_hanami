a2b0850aaa95a3ed258be7a6f6d7a7410fc58479
Carolyn Cole <cac9@princeton.edu>
2026-05-29 11:33:37 -0400

    Working New form

--- bookshelf/app/views/books/new.rb
+++ bookshelf/app/views/books/new.rb
@@ -4,6 +4,12 @@ module Bookshelf
   module Views
     module Books
       class New < Bookshelf::View
+        include Deps["repos.book_repo"]
+
+        expose :form_submit, default: "Create Book"
+        expose :book do |context:|
+          context.request.params[:book]
+        end
       end
     end
   end
