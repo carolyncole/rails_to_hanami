91164ccee4db24daa92b57e401e08a45cf13a63b
Carolyn Cole <cac9@princeton.edu>
2026-05-29 09:13:58 -0400

    Adding working code for intial setup

---
+++ bookshelf/app/repos/book_repo.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Bookshelf
+  module Repos
+    class BookRepo < Bookshelf::DB::Repo
+      def create(attributes)
+        attributes[:created_at] = Time.now
+        attributes[:updated_at] = Time.now
+        books.changeset(:create, attributes).commit
+      end
+          
+      def last = books.last
+      def get(id) = books.by_pk(id).one!
+    end
+  end
+end
---
+++ bookshelf/spec/system/book_show_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+RSpec.describe "book/", type: :system do
+
+    it "visits the home page and shows a welcome message" do
+      book = Bookshelf::Repos::BookRepo.new.create(title: "book 1", author: "author 1")
+      Bookshelf::Repos::BookRepo.new.create(title: "book 2", author: "author 2")
+      visit "/books/#{book.id}"
+  
+      expect(page).to have_content "Title: book 1"
+      expect(page).not_to have_content "Title: book 2"
+      expect(page).to have_content "Author: author 1"
+      expect(page).not_to have_content "Author: author 2"
+    end
+  end
