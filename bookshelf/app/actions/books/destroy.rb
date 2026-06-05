d2d526895c050b62eb313e2d083f61731903e2d1
Carolyn Cole <cac9@princeton.edu>
2026-06-05 15:06:25 -0400

    Working Destroy

--- bookshelf/app/actions/books/destroy.rb
+++ bookshelf/app/actions/books/destroy.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
-
 module Bookshelf
   module Actions
     module Books
       class Destroy < Bookshelf::Action
+        include Deps["repos.book_repo"]
+
+        params do
+          required(:id).filled(:integer)
+        end
+
         def handle(request, response)
+          # in handle method
+          result = book_repo.delete(request.params[:id])
+          response.flash[:notice] = "Book was successfully destroyed"
+          response.redirect_to routes.path(:books)
         end
       end
     end
