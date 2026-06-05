2abc2a7d3317edf3d62f105c6c63ecbdfc9e09f4
Carolyn Cole <cac9@princeton.edu>
2026-06-05 09:49:53 -0400

    Updated the application to allow for a session to get flash message working

--- bookshelf/config/settings.rb
+++ bookshelf/config/settings.rb
@@ -5,5 +5,6 @@ module Bookshelf
     # Define your app settings here, for example:
     #
     # setting :my_flag, default: false, constructor: Types::Params::Bool
+    setting :session_secret, constructor: Types::String, default: "____local_development_secret_only____local_development_secret_only___"
   end
 end
