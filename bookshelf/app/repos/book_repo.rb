da18195994322e69ef8287c2b2eeecb300e40378
Carolyn Cole <cac9@princeton.edu>
2026-06-12 08:16:07 -0400

    Working database Rails database in a Hanami application Reltation and Repository built Repository has all methods for accessing the Rails data

---
+++ bookshelf/app/repos/book_repo.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Bookshelf
+  module Repos
+    class BookRepo < Bookshelf::DB::Repo
+      def all = books.to_a
+
+      def create(attributes)
+        attributes[:created_at] = Time.now
+        attributes[:updated_at] = Time.now
+        books.changeset(:create, attributes).commit
+      end
+
+      def count = books.count
+
+      def delete(id)
+        books.by_pk(id).changeset(:delete).commit
+      end
+
+      def get(id) = books.by_pk(id).one!
+
+      def last = books.last
+
+      def update(id, attributes)
+        attributes[:updated_at] = Time.now
+        books.by_pk(id).changeset(:update, attributes).commit
+      end
+    end
+  end
+end
